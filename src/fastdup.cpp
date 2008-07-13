4bbec61c976e579ff666217c6a4c5ade0327806e
John Brooks <special@dereferenced.net>
2008-07-13 04:25:18 +0000

    Added option parsing and such
    git-svn-id: file://localhost/home/burchr/fastdup/fastdup@18 9fb5eb53-dc21-4a61-961c-56d408ae83b2

--- src/fastdup.cpp
+++ src/fastdup.cpp
@@ -1,54 +1,80 @@
 #include "main.h"
+#include <getopt.h>
 
 std::map<off_t,FileReference*> SizeMap;
 std::vector<FileReference*> SizeDups;
 int DupeCount = 0, DupeSetCount = 0, FileCount = 0;
+bool Interactive = false;
 
 int treecount = 0;
 char **scantrees = NULL;
 
-int main(int argc, char **argv)
+bool ReadOptions(int argc, char **argv)
 {
-	if (argc < 2)
+	Interactive = isatty(fileno(stdout));
+	
+	char opt;
+	while ((opt = getopt(argc, argv, "ib")) >= 0)
 	{
-		printf("Usage: %s <directory>\n", argv[0]);
-		return EXIT_FAILURE;
+		switch (opt)
+		{
+			case 'i':
+				Interactive = true;
+				break;
+			case 'b':
+				Interactive = false;
+				break;
+			default:
+				return false;
+		}
 	}
 	
+	if (optind >= argc)
 	{
-		char cwd[PATH_MAX];
-		char tmp[PATH_MAX];
-		if (!getcwd(cwd, PATH_MAX))
+		printf("Usage: %s [options] directory [directory..]\n", argv[0]);
+		return false;
+	}
+	
+	char cwd[PATH_MAX];
+	char tmp[PATH_MAX];
+	if (!getcwd(cwd, PATH_MAX))
+	{
+		printf("Unable to get current directory: %s\n", strerror(errno));
+		return false;
+	}
+	
+	treecount = argc - optind;
+	scantrees = new char*[treecount];
+	for (int i = optind, j = 0; i < argc; i++, j++)
+	{
+		if (argv[i][0] != '/')
 		{
-			printf("Unable to get current directory: %s\n", strerror(errno));
-			return EXIT_FAILURE;
+			PathMerge(tmp, PATH_MAX, cwd, argv[i]);
+			int len = strlen(tmp) + 2;
+			scantrees[j] = new char[len];
+			PathResolve(scantrees[j], len, tmp);
+		}
+		else
+		{
+			int len = strlen(argv[i]);
+			scantrees[j] = new char[len + 2];
+			PathResolve(scantrees[j], len + 2, argv[i]);
 		}
 		
-		treecount = argc - 1;
-		scantrees = new char*[treecount];
-		for (int i = 1; i < argc; i++)
+		if (!DirectoryExists(scantrees[j]))
 		{
-			if (argv[i][0] != '/')
-			{
-				PathMerge(tmp, PATH_MAX, cwd, argv[i]);
-				int len = strlen(tmp) + 2;
-				scantrees[i - 1] = new char[len];
-				PathResolve(scantrees[i - 1], len, tmp);
-			}
-			else
-			{
-				int len = strlen(argv[i]);
-				scantrees[i - 1] = new char[len + 2];
-				PathResolve(scantrees[i - 1], len + 2, argv[i]);
-			}
-			
-			if (!DirectoryExists(scantrees[i - 1]))
-			{
-				printf("Error: %s does not exist or is not a directory\n", argv[i]);
-				return EXIT_FAILURE;
-			}
+			printf("Error: %s does not exist or is not a directory\n", argv[i]);
+			return false;
 		}
 	}
+	
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	if (!ReadOptions(argc, argv))
+		return EXIT_FAILURE;
 	
 	/* Initial scan - this step will recurse through the directory tree(s)
 	 * and find each file we will be working with. These files are mapped
