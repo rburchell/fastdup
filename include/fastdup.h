d508d839f129f008f6f83e0270ab297c00613129
John Brooks <special@dereferenced.net>
2008-10-20 23:58:21 +0000

    License change to GPLv3 - the modified BSD was an experiment, and it created problems sooner than I expected. The GPL is a bit more controlling than I would prefer, but does incorporate certain aspects I require that aren't present in BSD-style licenses.
    git-svn-id: file://localhost/home/burchr/fastdup/fastdup@39 9fb5eb53-dc21-4a61-961c-56d408ae83b2

--- include/fastdup.h
+++ include/fastdup.h
@@ -1,6 +1,23 @@
 #ifndef FASTDUP_H
 #define FASTDUP_H
 
+/* FastDup (http://dev.dereferenced.net/fastdup/)
+ * Copyright 2008 - John Brooks <special@dereferenced.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of the GNU General Public License is available in the
+ * LICENSE file distributed with this program.
+ */
+
 #include <map>
 
 class FastDup
--- include/main.h
+++ include/main.h
@@ -1,34 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Copyright (c) 2008, John Brooks <special@dereferenced.net>
- * All rights reserved.
+/* FastDup (http://dev.dereferenced.net/fastdup/)
+ * Copyright 2008 - John Brooks <special@dereferenced.net>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * The name of dereferenced.net, FastDup, or its contributors may not be
- *       used to endorse or promote products derived from this software without
- *       specific prior written permission, with the exception of simple
- *       attribution.
- *     * Redistribution, in whole or in part, with or without modification, for
- *       or as part of a commercial product or venture is prohibited without
- *       explicit written consent from the author.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * THIS SOFTWARE IS PROVIDED BY JOHN BROOKS ''AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of the GNU General Public License is available in the
+ * LICENSE file distributed with this program.
  */
 
 #define FASTDUP_VERSION "0.2"
